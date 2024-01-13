26fdc71acc74cfc43111a29f58e2d1116f7a856f
saptadipa77 <saptadipadatta5@gmail.com>
2024-01-13 13:19:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const express = require('express')
+// import express from "express"
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/twitter',(req,res)=>{
+    res.send('hiteshdotcom')
+})
+app.get('/login',(req,res)=>{
+    res.send('<h1>please login</h1>')
+})
+app.get('/youtube',(req,res)=>{
+  res.send("<h2>chai</h2>")
+})
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
